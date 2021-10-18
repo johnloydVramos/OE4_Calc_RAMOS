a423c114aeeab0a7c5e4575859987225ec9b8c5c
johnloydVramos <ramos.johnloyd2@gmail.com>
2021-10-18 09:08:32 +0800

    commit

---
+++ OE4_calc.php
@@ -0,0 +1,326 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class="supernova"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="alternate" type="application/json+oembed" href="https://www.jotform.com/oembed/?format=json&amp;url=https%3A%2F%2Fform.jotform.com%2F212897607501459" title="oEmbed Form">
+<link rel="alternate" type="text/xml+oembed" href="https://www.jotform.com/oembed/?format=xml&amp;url=https%3A%2F%2Fform.jotform.com%2F212897607501459" title="oEmbed Form">
+<meta property="og:title" content="Pixel Density Calculator (PPI Calculator)" >
+<meta property="og:url" content="https://form.jotform.com/212897607501459" >
+<meta property="og:description" content="Please click the link to complete this form." >
+<meta name="slack-app-id" content="AHNMASS8M">
+<link rel="shortcut icon" href="https://cdn.jotfor.ms/assets/img/favicons/favicon-2021.svg">
+<link rel="canonical" href="https://form.jotform.com/212897607501459" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=1" />
+<meta name="HandheldFriendly" content="true" />
+<title>Outcomes Evaluation 4 Calculator</title>
+<link href="https://cdn01.jotfor.ms/static/formCss.css?3.3.28507" rel="stylesheet" type="text/css" />
+<style type="text/css">@media print{.form-section{display:inline!important}.form-pagebreak{display:none!important}.form-section-closed{height:auto!important}.page-section{position:initial!important}}</style>
+<link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/css/styles/nova.css?3.3.28507" />
+<link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/themes/CSS/566a91c2977cdfcd478b4567.css?themeRevisionID=5f6c4c83346ec05354558fe8"/>
+<link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/css/styles/payment/payment_feature.css?3.3.28507" />
+<style type="text/css">
+    .form-label-left{
+        width:150px;
+    }
+    .form-line{
+        padding-top:12px;
+        padding-bottom:12px;
+    }
+    .form-label-right{
+        width:150px;
+    }
+    body, html{
+        margin:0;
+        padding:0;
+        background:#fff;
+    }
+
+    .form-all{
+        margin:0px auto;
+        padding-top:0px;
+        width:690px;
+        color:#555 !important;
+        font-family:"Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Verdana, sans-serif;
+        font-size:14px;
+    }
+    .form-radio-item label, .form-checkbox-item label, .form-grading-label, .form-header{
+        color: #555;
+    }
+
+</style>
+
+<style type="text/css" id="form-designer-style">
+    /* Injected CSS Code */
+/*PREFERENCES STYLE*/
+    .form-all {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-all .qq-upload-button,
+    .form-all .form-submit-button,
+    .form-all .form-submit-reset,
+    .form-all .form-submit-print {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-all .form-pagebreak-back-container,
+    .form-all .form-pagebreak-next-container {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-header-group {
+      font-family: Lucida Grande, sans-serif;
+    }
+    .form-label {
+      font-family: Lucida Grande, sans-serif;
+    }
+  
+    .form-label.form-label-auto {
+      
+    display: inline-block;
+    float: left;
+    text-align: left;
+  
+    }
+  
+    .form-line {
+      margin-top: 12px;
+      margin-bottom: 12px;
+    }
+  
+    .form-all {
+      max-width: 500px;
+      width: 100%;
+    }
+  
+    .form-label.form-label-left,
+    .form-label.form-label-right,
+    .form-label.form-label-left.form-label-auto,
+    .form-label.form-label-right.form-label-auto {
+      width: 200px;
+    }
+  
+    .form-all {
+      font-size: 14px
+    }
+    .form-all .qq-upload-button,
+    .form-all .qq-upload-button,
+    .form-all .form-submit-button,
+    .form-all .form-submit-reset,
+    .form-all .form-submit-print {
+      font-size: 14px
+    }
+    .form-all .form-pagebreak-back-container,
+    .form-all .form-pagebreak-next-container {
+      font-size: 14px
+    }
+  
+    .supernova .form-all, .form-all {
+      background-color: ;
+    }
+  
+    .form-all {
+      color: #555;
+    }
+    .form-header-group .form-header {
+      color: #555;
+    }
+    .form-header-group .form-subHeader {
+      color: #555;
+    }
+    .form-label-top,
+    .form-label-left,
+    .form-label-right,
+    .form-html,
+    .form-checkbox-item label,
+    .form-radio-item label {
+      color: #555;
+    }
+    .form-sub-label {
+      color: #6f6f6f;
+    }
+  
+    .supernova {
+      background-color: undefined;
+    }
+    .supernova body {
+      background: transparent;
+    }
+  
+    .form-textbox,
+    .form-textarea,
+    .form-dropdown,
+    .form-radio-other-input,
+    .form-checkbox-other-input,
+    .form-captcha input,
+    .form-spinner input {
+      background-color: undefined;
+    }
+  
+    .supernova {
+      background-image: none;
+    }
+    #stage {
+      background-image: none;
+    }
+  
+    .form-all {
+      background-image: none;
+    }
+  
+  .ie-8 .form-all:before { display: none; }
+  .ie-8 {
+    margin-top: auto;
+    margin-top: initial;
+  }
+  
+  /*PREFERENCES STYLE*//*__INSPECT_SEPERATOR__*/.form-all {
+border-top: 6px solid #555;
+box-shadow: 0 0 10px #555;
+}
+    /* Injected CSS Code */
+</style>
+
+<script src="https://cdn02.jotfor.ms/static/prototype.forms.js" type="text/javascript"></script>
+<script src="https://cdn03.jotfor.ms/static/jotform.forms.js?3.3.28507" type="text/javascript"></script>
+<script src="https://cdn01.jotfor.ms/js/vendor/math-processor.js?v=3.3.28507" type="text/javascript"></script>
+<script type="text/javascript">	JotForm.newDefaultTheme = false;
+	JotForm.extendsNewTheme = false;
+	JotForm.newPaymentUIForNewCreatedForms = false;
+
+   JotForm.setCalculations([{"decimalPlaces":"2","equation":"[(({1}^2+{3}^2)~2)\u002F{4}]","insertAsText":"","newCalculationType":"1","operands":"1,3,4","readOnly":"1","resultField":"6","showBeforeInput":"","showEmptyDecimals":""}]);
+	JotForm.init(function(){
+	/*INIT-START*/
+if (window.JotForm && JotForm.accessible) $('input_1').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_3').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_4').setAttribute('tabindex',0);
+	/*INIT-END*/
+	});
+
+   JotForm.prepareCalculationsOnTheFly([null,{"name":"widthpixels","qid":"1","text":"Width (pixels)","type":"control_textbox"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"heightpixels","qid":"3","text":"Height (pixels)","type":"control_textbox"},{"name":"diagonalSize","qid":"4","text":"Diagonal Size (inches)","type":"control_textbox"},null,{"name":"calculatedPixel","qid":"6","text":"Calculated Pixel Density (PPI)","type":"control_calculation"},{"name":"clickTo","qid":"7","text":"Pixel Density Calculator","type":"control_head"}]);
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"widthpixels","qid":"1","text":"Width (pixels)","type":"control_textbox"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"heightpixels","qid":"3","text":"Height (pixels)","type":"control_textbox"},{"name":"diagonalSize","qid":"4","text":"Diagonal Size (inches)","type":"control_textbox"},null,{"name":"calculatedPixel","qid":"6","text":"Calculated Pixel Density (PPI)","type":"control_calculation"},{"name":"clickTo","qid":"7","text":"Pixel Density Calculator","type":"control_head"}]);}, 20); 
+</script>
+</head>
+<body>
+<form class="jotform-form" method="post" name="form_212897607501459" id="212897607501459" accept-charset="utf-8" autocomplete="on">
+  <!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+ini_set('display_errors',0);
+ 
+if( isset( $_REQUEST['calculate'] ))
+{
+$operator=$_REQUEST['operator'];
+$n1 = $_REQUEST['first_value'];
+$n2 = $_REQUEST['second_value'];
+ 
+if($operator=="+")
+{
+$res= $n1+$n2;
+}
+if($operator=="-")
+{
+$res= $n1-$n2;
+}
+if($operator=="*")
+{
+$res =$n1*$n2;
+}
+if($operator=="/")
+{
+$res= $n1/$n2;
+}
+if($operator=="√" )
+{
+$res= sqrt($n1);
+}
+if($operator=="%" )
+{
+$res= $n1 % $n2;
+}
+
+ 
+if($_REQUEST['first_value']==NULL || $_REQUEST['second_value']==0)
+{
+echo "<scrip language=javascript> alert(\"Please Enter Correct values.\");</script>";
+}
+}
+?>
+
+<form><center>
+    <body>
+    <div id="calculator">
+<table style="border:groove blue">
+ 
+<tr>
+<td style="background-color:turquoise; color:black; font-family:'Times New Roman'">Enter Number</td>
+<td colspan="2">
+<input name="first_value" type="text" style="color:blue;"/></td>
+</tr>
+ 
+<tr>
+<td style="color:blue; font-family:'Times New Roman; width: 100%">Select Operator</td>
+<td>
+<select name="operator" style="width: 100px">
+<option>+</option>
+<option>-</option>
+<option>*</option>
+<option>/</option>
+<option>√</option>
+<option>%</option>
+</select></td>
+</tr>
+ 
+<tr>
+<td style="background-color:turquoise; color:black; font-family:'Times New Roman'">Enter Number</td>
+<td class="auto-style5">
+<input name="second_value" type="text"  style="color:red"/></td> 
+</tr>
+ 
+<tr>
+<td></td>
+<td><input type="submit" name="calculate" value="Calculate" style="color:wheat;background-color:red" /></td>	 
+</tr>
+ 
+<tr>
+<td style="background-color:turquoise;color:black">Output = </td>
+<td style="color:darkblue"><?php echo $res;?></td>
+</tr>	
+ 
+</table>
+</center>
+</form>
+</body>
+</body>
+</html>
+</div>
+      
+  <script>
+  JotForm.showJotFormPowered = "new_footer";
+  </script>
+  <script>
+  JotForm.poweredByText = "Powered by Jotform";
+  </script>
+  <input type="hidden" class="simple_spc" id="simple_spc" name="simple_spc" value="212897607501459" />
+  <script type="text/javascript">
+  var all_spc = document.querySelectorAll("form[id='212897607501459'] .si" + "mple" + "_spc");
+for (var i = 0; i < all_spc.length; i++)
+{
+  all_spc[i].value = "212897607501459-212897607501459";
+}
+  </script>
+  <div class="formFooter-heightMask">
+  </div>
+  <div class="formFooter f6 branding21">
+    <div class="formFooter-wrapper formFooter-leftSide">
+      <a href="https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=212897607501459&utm_content=jotform_logo&utm_campaign=powered_by_jotform_le" target="_blank" class="formFooter-logoLink"><img class="formFooter-logo" src="https://cdn.jotfor.ms/assets/img/logo2021/jotform-logo-white.svg" alt="Jotform Logo" style="height: 44px;"></a>
+    </div>
+    <div class="formFooter-wrapper formFooter-rightSide">
+      <span class="formFooter-text">
+        Now create your own Jotform - It's free!
+      </span>
+      <a class="formFooter-button" href="https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=212897607501459&utm_content=jotform_button&utm_campaign=powered_by_jotform_le" target="_blank">Create your own Jotform</a>
+    </div>
+  </div>
+</form></body>
+</html>
+<script type="text/javascript">JotForm.ownerView=true;</script>
